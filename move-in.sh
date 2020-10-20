166059d2fc8b86e0f054485dc0c0b29350f4bba2
Alex <atanelli@protonmail.com>
2020-10-20 14:08:22 -0400

    Create move-in.sh

---
+++ move-in.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -eu
+
+rm -rf ~/tmp/movein
+mkdir -p ~/tmp/movein
+pushd ~/tmp/movein
+
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install \
+  vault jq kubectl kubernetes-helm \
+  go git tcpdump tmux tmate docker sipcalc tree watch \
+  tldr bat tig coreutils
+brew cask install \
+  visual-studio-code emacs iterm2 slack tunnelblick google-chrome \
+  zoomus docker
+
+popd ~/tmp/movein
+rm -rf ~/tmp/movein
