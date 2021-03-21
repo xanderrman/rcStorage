b41f8f7a154e797628be84f1dbcd4d885176c0e8
Alex <atanelli@protonmail.com>
2021-03-21 18:07:48 -0400

    Update move-in.sh

--- move-in.sh
+++ move-in.sh
@@ -7,13 +7,22 @@ pushd ~/tmp/movein
 
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 brew install \
-  vault jq fx yh kubectl kubectx kubernetes-helm \
-  go git tcpdump tmux tmate docker sipcalc tree watch \
-  tldr bat tig coreutils ripgrep pyenv pyenv-virtualenv
+  # CLI tools
+  jq jsonlint fx yh yamllint tcpdump tmux tmate sipcalc tree watch tldr bat tig ripgrep kubectx htop \
+  # big boi CLI clients
+  vault kubernetes-cli helm vault terraform docker mysql \
+  #programmer things
+  go git neovim
+
 brew install --cask \
   visual-studio-code emacs iterm2 slack tunnelblick google-chrome \
-  zoomus docker
+  zoomus docker muzzle spotify tableplus rectangle vagrant
 
 popd ~/tmp/movein
 rm -rf ~/tmp/movein
+
+echo "you're most of the way there, you just gotta go download some stuff still..."
+echo "https://www.nerdfonts.com/font-downloads"
+echo "https://github.com/lensapp/lens/releases"
